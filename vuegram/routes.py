1d83456a5941fb8050504836cf947c25239c36ea
gmork2 <gmork.02@gmail.com>
2021-05-16 20:03:21 +0200

    Added cors setup in routes

--- vuegram/routes.py
+++ vuegram/routes.py
@@ -1,9 +1,21 @@
 import pathlib
+import aiohttp_cors
 
 PROJECT_ROOT = pathlib.Path(__file__).parent
 
 
 def setup_routes(app):
+    cors = aiohttp_cors.setup(
+        app,
+        defaults={
+            "*": aiohttp_cors.ResourceOptions(
+                allow_credentials=True,
+                expose_headers="*",
+                allow_headers="*",
+            )
+        }
+    )
+
     app.router.add_static(
         '/static/',
         path=PROJECT_ROOT / 'static',
