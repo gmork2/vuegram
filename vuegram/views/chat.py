cfd5f22db2463373ecc206c121e8fbdb364d9b45
gmork2 <gmork.02@gmail.com>
2021-05-18 16:00:49 +0200

    Added get_anonymous_user function

--- vuegram/views/chat.py
+++ vuegram/views/chat.py
@@ -4,3 +4,11 @@ import random
 from aiohttp import web
 
 log = logging.getLogger(__name__)
+
+
+def get_anonymous_user():
+    user_id = random.randint(1, 101)
+    return {
+        'id': user_id,
+        'name': 'Anonymous' + str(user_id)
+    }
