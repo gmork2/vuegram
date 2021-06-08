69244191373db33ede703f1b3a0a8b1f7f4f988d
gmork2 <gmork.02@gmail.com>
2021-06-09 00:05:54 +0200

    Implemented message model

--- vuegram/static/js/models/message.js
+++ vuegram/static/js/models/message.js
@@ -27,7 +27,52 @@ class BaseMessage {
 }
 
 class Message extends BaseMessage{
+    static MIN_ELAPSED_TIME = 60
+    static MAX_ELAPSED_TIME = 3600
 
+    /**
+     * Represents a message.
+     * @constructor
+     * @param {Object} data - Object data.
+     */
+    constructor(data) {
+        super(data)
+
+        this._title = data.title || data._title
+        this.text = data.text || ''
+        this.read = true
+        this.timestamp = Date.now()
+        this.date = DEFAULT_DATE
+    }
+
+    get title() {
+        return this._title || this.user.name
+    }
+
+    set title(value) {
+        this._title = value
+    }
+
+    /**
+     * Updates the string date according elapsed time.
+     * @returns {number}
+     */
+    updateDate() {
+        const elapsed = this.getElapsedTime()
+
+        if (elapsed >= Message.MIN_ELAPSED_TIME)
+            if (elapsed < Message.MAX_ELAPSED_TIME)
+                this.date = Math.trunc(elapsed / 60) + ' min ago'
+            else
+                this.date = this.date.toISOString().substr(11, 5)
+
+        return elapsed
+    }
+
+    getElapsedTime() {
+        const date = new Date(this.timestamp)
+        return Math.trunc((Date.now() - date) / 1000)
+    }
 }
 
 export default Message
