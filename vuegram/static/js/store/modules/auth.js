d54869207cda04ae239c31f0d8a16b743ee0af0d
gmork2 <gmork.02@gmail.com>
2021-06-09 00:11:31 +0200

    Implemented auth module to store user in local storage

--- vuegram/static/js/store/modules/auth.js
+++ vuegram/static/js/store/modules/auth.js
@@ -1,11 +1,28 @@
+import User from "../../models/user.js"
+
 export default {
     namespaced: true,
 
     state() {
-
+        return {
+            user: {},
+        }
     },
 
     mutations: {
+        SET_USER(state, user) {
+            state.user = user
+        },
 
+        LOAD_USER(state) {
+            const user = localStorage.getItem('user')
+            this.commit("auth/SET_USER", new User(user))
+        },
+
+        SAVE_USER(state, user) {
+            if (localStorage.getItem("user") === null)
+                localStorage.setItem('user', JSON.stringify(user))
+            this.commit("auth/SET_USER", user)
+        },
     },
 }
