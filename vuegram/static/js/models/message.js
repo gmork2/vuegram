4158b054bc9cffdcd3ce54e1d8398c676122815c
gmork2 <gmork.02@gmail.com>
2021-06-08 21:57:48 +0200

    Created base class for message model

---
+++ vuegram/static/js/models/message.js
@@ -0,0 +1,27 @@
+import User from "./user.js"
+
+const DEFAULT_DATE = 'now'
+
+class BaseMessage {
+    /**
+     * Represents a message.
+     * @constructor
+     * @param {Object} data - Object data.
+     */
+    constructor(data) {
+        if (!'user' in data)
+            throw new ReferenceError("Message should has 'user' property!")
+
+        this.user = new User(data.user)
+        this.id = data.hasOwnProperty('id') ? data.id : Message.createId(this.user)
+    }
+
+    /**
+     * Returns a new user id.
+     * @param {User} user - Current logged user
+     * @returns {string}
+     */
+    static createId(user) {
+        return `${user.id + Date.now()}`
+    }
+}
