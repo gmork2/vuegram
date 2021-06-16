322e5214ff4550902f7eb6ab08b0feca49d5e46b
gmork2 <gmork.02@gmail.com>
2021-06-16 15:30:02 +0200

    Added socket module to store

--- vuegram/static/js/store/store.js
+++ vuegram/static/js/store/store.js
@@ -1,11 +1,13 @@
 import Auth from "./modules/auth.js"
 import Messages from "./modules/messages.js"
+import Socket from "./modules/socket.js"
 
 export default Vuex.createStore({
 
     modules: {
         auth: Auth,
         messages: Messages,
+        socket: Socket
     },
 
     // plugins: [Vuex.createLogger()]
