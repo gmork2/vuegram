b6a9e68d03e2fd4b3c503ddfeff6928dce8b5aa4
gmork2 <gmork.02@gmail.com>
2021-06-16 15:28:15 +0200

    Implemented a simplified messages module for the vuex store

--- vuegram/static/js/store/modules/messages.js
+++ vuegram/static/js/store/modules/messages.js
@@ -11,7 +11,60 @@ export default {
         }
     },
 
-    getters: {},
+    getters: {
+        unread: (state) => state.filter((message) => !message.read),
 
-    mutations: {}
+        unreadFrom: (state, getters) => getters.unread
+            .map((message) => message.user.name)
+    },
+
+    mutations: {
+        INCREMENT_INDEX(state) {
+            state.index++
+        },
+
+        ADD_MESSAGES(state, messages) {
+            const arr = JSON.parse(messages)
+            arr.forEach(msg => {
+                this.commit('messages/ADD_MESSAGE', msg)
+            })
+        },
+
+        LOAD_MESSAGES(state) {
+            const messages = localStorage.getItem('messages')
+            if (messages)
+                this.commit('messages/ADD_MESSAGES', messages)
+        },
+
+        SAVE_MESSAGES(state, index) {
+            const lastMessages = state.messages.slice(-index)
+            if (lastMessages)
+                localStorage.setItem(
+                    'messages',
+                    JSON.stringify(lastMessages)
+                )
+        },
+
+        UPDATE_MESSAGES(state) {
+            const arr = state.messages.slice(state.index)
+            arr.forEach((message) => {
+                const elapsed = message.updateDate()
+                if (elapsed >= Message.MAX_ELAPSED_TIME)
+                    this.commit("messages/INCREMENT_INDEX")
+            })
+        },
+
+        CLEAR_MESSAGES(state) {
+            state.messages.forEach(msg => msg.id = null)
+            setTimeout(() => state.messages = [], 500)
+            localStorage.removeItem('messages')
+            state.index = 0
+        },
+
+        ADD_MESSAGE(state, data) {
+            const msg = new Message(data)
+            this.counter++
+            state.messages.push(msg)
+        },
+    },
 }
