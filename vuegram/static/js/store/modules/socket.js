fe6e8b5e4befc1661edb8d07f65c100d3872dec6
gmork2 <gmork.02@gmail.com>
2021-06-17 10:01:59 +0200

    Implemented websockets client

--- vuegram/static/js/store/modules/socket.js
+++ vuegram/static/js/store/modules/socket.js
@@ -10,9 +10,78 @@ export default {
     },
 
     getters: {
-        // ../../utils/socket/status.js
+        // Enum -> ../../utils/socket/status.js
         status: (state) => state.ws ? 'connected' : 'disconnected',
     },
 
-    mutations: {}
+    mutations: {
+        SET_CONNECTION(state, uri) {
+            if (state.ws == null) {
+                state.ws = new WebSocket(uri)
+                state.ws.onopen = () => {}
+                state.ws.onmessage = (e) => {
+                    const data = JSON.parse(e.data)
+                    switch (data.action) {
+                        case 'connect':
+                            this.commit("auth/SAVE_USER", new User(data.user))
+                            this.commit("messages/LOAD_MESSAGES")
+                            this.commit("messages/ADD_MESSAGE", {
+                                ...{title: 'Connected as ' + data.user.name},
+                                ...data
+                            })
+                            break
+                        case 'disconnect':
+                            this.commit("messages/ADD_MESSAGE", {
+                                ...{title: 'Disconnected as ' + data.user.name},
+                                ...data
+                            })
+                            break
+                        case 'join':
+                            this.commit("messages/ADD_MESSAGE", {
+                                ...{title: 'Joined ' + data.user.name},
+                                ...data
+                            })
+                            break
+                        case 'sent':
+                            this.commit("messages/ADD_MESSAGE", {
+                                ...{title: data.user.name},
+                                ...data
+                            })
+                            break
+                    }
+                }
+                state.ws.onclose = () => {
+                    const user = localStorage.getItem("user")
+                    state.ws = null
+                    if (user)
+                        this.commit("messages/ADD_MESSAGE", {
+                            title: 'Disconnected', user: new User(user)
+                        })
+                }
+            }
+        },
+
+        CLOSE_CONNECTION(state) {
+            if (state.ws) {
+                state.ws.close()
+                state.ws = null
+            }
+        }
+    },
+
+    actions: {
+        sendMessage({commit, state}, text) {
+            if (state.ws) {
+                state.ws.send(text)
+            } else {
+                // disconnected!
+            }
+
+            this.commit("messages/ADD_MESSAGE", {
+                title: this.state.auth.user.name,
+                text: text,
+                user: this.state.auth.user
+            })
+        }
+    },
 }
