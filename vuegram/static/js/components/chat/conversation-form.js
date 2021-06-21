44bed7361c04aded40b213ecb362645ed882f379
gmork2 <gmork.02@gmail.com>
2021-06-21 15:17:18 +0200

    Created vg-conversation-form component

---
+++ vuegram/static/js/components/chat/conversation-form.js
@@ -0,0 +1,29 @@
+export default {
+    template: `
+        <div id="vg-conversation-form" class="border-left-0">
+            <div class="d-flex">
+                
+            </div>
+        </div>
+    `,
+
+    name: 'vg-conversation-form',
+
+    data() {
+        return {
+            inputText: ''
+        }
+    },
+
+    props: {
+        status: String
+    },
+
+    computed: {
+
+    },
+
+    methods: {
+
+    },
+}
