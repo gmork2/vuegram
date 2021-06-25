e3ed33df259914c932bcce749b2843b85b17f79c
gmork2 <gmork.02@gmail.com>
2021-06-25 16:40:03 +0200

    Set scroll button transition in template

--- vuegram/static/js/components/chat/buttons/vg-scroll-button.js
+++ vuegram/static/js/components/chat/buttons/vg-scroll-button.js
@@ -1,6 +1,16 @@
 export default {
     template: `
-
+        <transition
+            name="custom-classes-transition"
+            enter-active-class="animate__animated animate__fadeInRight"
+            leave-active-class="animate__animated animate__fadeOutRight">
+            <span class="vg-circle overflow-hidden">
+                <i
+                    class="ri-arrow-down-s-line"
+                    @click="setAction">
+                </i>
+            </span>
+        </transition>
     `,
 
     name: 'vg-scroll-button',
